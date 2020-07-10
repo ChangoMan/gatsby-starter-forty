81e0e9e6ff6bd4b22043c7bdd301f8e2823a1e8c
ujely <67135051+ujely@users.noreply.github.com>
2020-07-09 19:01:52 -0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,12 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+            <h1>Kanvas Hair Studio</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <h6>Where your hair is a masterpiece</h6>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Book Appointment</a></li>
                 </ul>
             </div>
         </div>
